9ea3169082c7f153782a181e95613a1428cf7a25
Dilan Madhusanka <dilanm@insharptechnologies.com>
2021-06-05 23:03:16 +0530

    introdcution to numpy

---
+++ numpy_tutorial.py
@@ -0,0 +1,31 @@
+import numpy as np
+import time
+import sys
+
+l = range(1000)
+# print(sys.getsizeof(5)*len(l))
+
+array = np.arange(1000)
+# print(array.size*array.itemsize)
+
+SIZE = 1000000
+
+l1 = range(SIZE)
+l2 = range(SIZE)
+
+a1 = np.arange(SIZE)
+a2 = np.arange(SIZE)
+
+# python list
+start = time.time()
+result = [(x+y) for x,y in zip(l1, l2)]
+
+# for x,y in zip(l1,l2) :
+#     print(x+y)
+
+print("python list took: ", (time.time() - start)*1000)
+
+# numpy array
+start = time.time()
+result = a1 + a2
+print("numpy took: ", (time.time()-start)*1000)
